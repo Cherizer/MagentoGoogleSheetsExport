4b62e69a3777ef128994a6f75f88c619f0034962
Einārs Sijāts <68387158+Cherizer@users.noreply.github.com>
2025-07-28 09:58:53 +0300

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @category    InGuru
+ * @package     MagentoGoogleSheetsExport
+ * @author      Einars <einars@inguru.lv>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MagentoGoogleSheetsExport',
+    __DIR__
+);
